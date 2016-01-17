8e553811e44c1fd6fc3124524638812d9a5c36d7
Joshua Harlow <harlowja@gmail.com>
2016-01-16 23:33:06 -0800

    Start to add personality and reviewer modules
    Change-Id: I687e810cf5e4c4d6212caa32a2ff659da4d34d8f

---
+++ nerdreviewer/reviewer.py
@@ -0,0 +1,39 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import abc
+
+import six
+
+
+@six.add_metaclass(abc.ABCMeta)
+class Reviewer(object):
+
+    def __init__(self, name=u"John Doe"):
+        self._name = name
+
+    @property
+    def name(self):
+        """Name of this reviewer (if any)."""
+        return self._name
+
+    @abc.abstractmethod
+    def review(self, a_review):
+        """Review some incoming review and return its analysis."""
+
+    @abc.abstractproperty
+    def description(self):
+        """Useful description of this reviewer (personality, type...)."""
+
+    @abc.abstractproperty
+    def personality(self):
+        """Current personality profile/dict of this reviewer"""
